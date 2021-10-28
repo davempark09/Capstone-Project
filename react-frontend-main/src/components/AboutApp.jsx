7715608912d25d505b79001afd684dbdc8a97558
davempark09 <43024345+davempark09@users.noreply.github.com>
2021-10-28 03:39:57 -0400

    Quick About Page Change

--- react-frontend-main/src/components/AboutApp.jsx
+++ react-frontend-main/src/components/AboutApp.jsx
@@ -9,7 +9,7 @@ class Aboutpage extends Component {
             </div>
             <div className="appInfo">
                 <h1>About the Application</h1>
-                <p>This application allows users to record/track their expenses and savings. Users can add, delete, update, and view their added entries made to hopefully make better financial decisions in the future.</p>
+                <p>A Fulltstack Expense/Savings management tool This application allows users to record, edit, delete, and view their expenses and saving. This application was created for people who have trouble organizing all their financial expenses and savings and to hopefully make better financial decisions. The front end uses React.js with CSS styling along with bootstrap/reactstrap. The back end is handled by Java and the database is handled by MySQL.</p>
                 <a className="clickLinks" href="https://github.com/davempark09/Capstone-Project">Click here for the application source code</a> </div>
         </div>
         );
